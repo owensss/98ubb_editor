1d1d369369a165b78e22c7b6bd71b9c7b8ff56ed
owensss <ownesss@gmail.com>
2013-11-30 13:54:56 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## A ubb editor for 98.
+
+All codes modified from ckeditor and its plugins.
---
+++ config.js
@@ -0,0 +1,24 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For the complete reference:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+        { name: 'about' },
+        { name: 'styles' }
+	];
+
+	// The default plugins included in the basic setup define some buttons that
+	// we don't want too have in a basic editor. We remove them here.
+	// config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+
+	// Let's have it basic on dialogs as well.
+	config.removeDialogTabs = 'link:advanced';
+    config.extraPlugins = 'font';
+};
